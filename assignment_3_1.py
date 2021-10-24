2725007f76a0738744c0a9a5283693b95ffc0f31
abiolla <abioladada100@gmail.com>
2021-10-24 15:44:09 -0400

    first commit of python for eveerybody

---
+++ assignment_3_1.py
@@ -0,0 +1,9 @@
+hrs = float(input('Enter Hours:'))
+rate = float(input('Enter rate per hour:'))
+
+if hrs <= 40 :
+    pay = hrs * rate
+    print(pay)
+else:
+    pay = (40 * rate ) + ((hrs - 40) * rate * 1.5)
+    print(pay)
---
+++ assignment_3_3.py
@@ -0,0 +1,14 @@
+score = float(input('Enter score:'))
+
+if score < 0.0 or score > 1:
+    print('Error, score is out of range')
+elif score < 0.6:
+    print('F')
+elif score < 0.7:
+    print("D")
+elif score < 0.8:
+    print("C")
+elif score < 0.9:
+    print("B")
+else:
+    print("A")
